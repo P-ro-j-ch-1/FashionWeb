031c21bc6644e9373b25b953eb35f1359fce4eac
Zafes <144411099+Zafes2806@users.noreply.github.com>
2025-10-08 21:13:58 +0700

    add getAllCategoryBlog (#17)

--- FashionAPI/src/controllers/allcodeController.js
+++ FashionAPI/src/controllers/allcodeController.js
@@ -24,18 +24,6 @@ let getAllCodeService = async (req, res) => {
         })
     }
 }
-let getAllCategoryBlog = async (req, res) => {
-    try {
-        let data = await allcodeService.getAllCategoryBlog(req.query.type);
-        return res.status(200).json(data);
-    } catch (error) {
-        console.log(error)
-        return res.status(200).json({
-            errCode: -1,
-            errMessage: 'Error from server'
-        })
-    }
-}
 let handleUpdateAllCode = async (req, res) => {
     try {
         let data = await allcodeService.handleUpdateAllCode(req.body);
@@ -75,6 +63,18 @@ let handleDeleteAllCode = async (req, res) => {
 let getListAllCodeService = async (req, res) => {
     try {
         let data = await allcodeService.getListAllCodeService(req.query);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+let getAllCategoryBlog = async (req, res) => {
+    try {
+        let data = await allcodeService.getAllCategoryBlog(req.query.type);
         return res.status(200).json(data);
     } catch (error) {
         console.log(error)

4cde05d5e8bce2dac58337f0c92a6c5793070f5d
thandieudaibip81 <tiencao8811@gmail.com>
2025-11-15 00:39:45 +0700

    Fix association to use correct model casing

--- FashionAPI/src/models/blog.js
+++ FashionAPI/src/models/blog.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            Blog.belongsTo(models.Allcode, { foreignKey: 'subjectId', targetKey: 'code', as: 'subjectData' })
+            Blog.belongsTo(models.allcode, { foreignKey: 'subjectId', targetKey: 'code', as: 'subjectData' })
         }
     };
     Blog.init({

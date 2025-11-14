aa2c642cebac7f8a2d10ff88655fe62e55df90d2
thandieudaibip81 <tiencao8811@gmail.com>
2025-11-15 00:28:44 +0700

    Update ProductDetail.js

--- FashionAPI/src/models/ProductDetail.js
+++ FashionAPI/src/models/ProductDetail.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            ProductDetail.belongsTo(models.Product, { foreignKey: 'productId', targetKey: 'id', as: 'productDetailData' })
-            ProductDetail.hasMany(models.ProductImage, { foreignKey: 'productdetailId', as: 'productImageData' })
+            ProductDetail.belongsTo(models.product, { foreignKey: 'productId', targetKey: 'id', as: 'productDetailData' })
+            ProductDetail.hasMany(models.productimage, { foreignKey: 'productdetailId', as: 'productImageData' })
         }
     };
     ProductDetail.init({
